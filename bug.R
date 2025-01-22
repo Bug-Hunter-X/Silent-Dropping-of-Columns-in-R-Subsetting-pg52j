d61f22c4cfcfac4277d7fc3333f80b7e7202d1c0
BugHunter-X <askzenai@gmail.com>
2025-01-22 22:02:07 +0100

    Add bug.R file

---
+++ bug.R
@@ -0,0 +1,8 @@
+```r
+# This code attempts to subset a data frame using a character vector
+# that contains invalid column names.
+
+df <- data.frame(A = 1:3, B = 4:6, C = 7:9)
+col_names <- c("A", "D", "E")
+subset_df <- df[, col_names]
+```
